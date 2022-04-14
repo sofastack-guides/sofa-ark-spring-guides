e3bc1f0be42a8e577af293765ce9673d10ccf236
yuanyuan <fengjun.zfj@antgroup.com>
2022-04-14 13:41:40 +0800

    init

---
+++ src/main/java/com/alipay/sofa/sofaarkspringguides/SofaArkSpringGuidesApplication.java
@@ -0,0 +1,13 @@
+package com.alipay.sofa.sofaarkspringguides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofaArkSpringGuidesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofaArkSpringGuidesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alipay/sofa/sofaarkspringguides/SofaArkSpringGuidesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alipay.sofa.sofaarkspringguides;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofaArkSpringGuidesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
